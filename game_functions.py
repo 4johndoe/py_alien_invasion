c842880bd6d0deeab2bc63a5222956501cd1d399
Богдан Демченко <C:\Users\Co_dbv\AppData\Roaming\The Bat!>
2016-11-09 08:32:46 +0200

    dropping the fleet and change direction

--- game_functions.py
+++ game_functions.py
@@ -102,8 +102,16 @@ def check_fleet_edges(ai_settings, aliens):
 		if alien.check_edges():
 			change_fleet_direction(ai_settings, aliens)
 			break
-# 278
+def change_fleet_direction(ai_settings, aliens):
+	"""Drop the entire fleet and change the fleet's direction."""
+	for alien in aliens.sprites():
+		alien.rect.y += ai_settings.fleet_drop_speed
+	ai_settings.fleet_direction *= -1
 
-def update_aliens(aliens):
-	"""Update the positions of all aliens in the fleet."""
+def update_aliens(ai_settings, aliens):
+	"""
+	Check if the fleet is at the an edge,
+	and the update the positions of all aliens in the fleet.
+	"""
+	check_fleet_edges(ai_settings, aliens)
 	aliens.update()
